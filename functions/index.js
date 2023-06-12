c1e2e2cd6b14bf87969d99f4c0954dd56f3aa0fe
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-06-12 21:06:39 +0700

    Replace old whitelisted addresses with new one
    This app will be for users that request a proxy and can't set up their own server.

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,7 @@ const corsServer = corsAnywhere.createServer({
     originWhitelist: [
       'http://localhost:3000',
       'http://localhost:5000',
-      'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com',
-      'https://test-my-api-endpoint.web.app',
-      'https://test-my-api-endpoint.firebaseapp.com'
+      'https://littlemomster.be', // for GitHub user @dboute
     ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
